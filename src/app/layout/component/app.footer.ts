eda9220291a404d92653fc1ee86804e299b58118
Pheasa <113016697+Pheasa-Uon@users.noreply.github.com>
2025-08-21 11:32:30 +0700

    Revert "Update app.footer.ts"
    This reverts commit c20d95cef6ca843a236a0f612e6e8abe051e114e.

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -3,27 +3,9 @@ import { Component } from '@angular/core';
 @Component({
     standalone: true,
     selector: 'app-footer',
-    template: `
-        <div class="layout-footer">
-            <div class="flex justify-between items-center md:w-1/2 w-full mx-auto px-4 py-2">
-
-                <!-- Left side: copyright -->
-                <div class="flex gap-2 items-center text-right">
-                    © Copyright 2019-2025. All Rights Reserved. Powered by
-                    <a href="https://primeng.org"
-                       target="_blank"
-                       rel="noopener noreferrer"
-                       class="text-primary font-bold hover:underline">
-                        Istar Business Intelligence Cambodia
-                    </a>
-                </div>
-
-                <!-- Right side: version -->
-                <div class="flex gap-2 items-center">
-                    version 1.0.0
-                </div>
-            </div>
-        </div>
-    `
+    template: `<div class="layout-footer">
+        © Copyright 2019-2025. All Rights Reserved. Power by
+        <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Istar Business Intelligence Cambodia</a>
+    </div>`
 })
 export class AppFooter {}
