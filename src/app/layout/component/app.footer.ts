929173de2291668608f0b0cd1d0c14e1c2b57aad
Pheasa <pheasa.uon@idealinkconsulting.com>
2025-09-14 15:17:29 +0700

    change keyword

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -5,10 +5,10 @@ import { Component } from '@angular/core';
     selector: 'app-footer',
     template: `<div class="layout-footer">
         <div class="flex justify-between items-center">
-            © Copyright 2019-2025. All Rights Reserved. Power by
+            © Copyright 2025-2025. All Rights Reserved. Power by
         </div>
         <div class="flex justify-between items-center">
-            <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Istar Business Intelligence Cambodia</a>
+            <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Istar Core Banking Cambodia</a>
         </div>
     </div>`
 })
